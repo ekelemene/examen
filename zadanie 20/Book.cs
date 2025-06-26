62c6d401336bb700d64d0e1ee2a5b0ba6a9b39b3
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 18:32:12 +0300

    Add files via upload

---
+++ zadanie 20/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Структура Book представляет книгу с основными свойствами
+    /// </summary>
+    struct Book
+    {
+        /// <summary>
+        ///  Приватные поля для хранения данных книги
+        /// </summary>
+        private string title;
+        private string author;
+        private int year;
+        private int pages;
+
+        /// <summary>
+        ///  Конструктор структуры для инициализации всех полей при создании объекта
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="author"></param>
+        /// <param name="year"></param>
+        /// <param name="pages"></param>
+        public Book(string title, string author, int year, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+            this.pages = pages;
+        }
+
+        /// <summary>
+        /// Метод для вывода информации о книге на консоль
+        /// </summary>
+        public void ShowInfo()
+        {
+            Console.WriteLine("Название: " + title);
+            Console.WriteLine("Автор: " + author);
+            Console.WriteLine("Год издания: " + year);
+            Console.WriteLine("Количество страниц: " + pages);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ zadanie 20/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя количество книг
+            Console.Write("Введите количество книг: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Создаём массив структур Book размером n
+            Book[] books = new Book[n];
+            int i; 
+            // Заполняем массив данными о каждой книге
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nВведите данные для книги " + (i + 1) + ":");
+                Console.Write("Название: ");
+                string title = Console.ReadLine();
+                Console.Write("Автор: ");
+                string author = Console.ReadLine();
+                Console.Write("Год издания: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Количество страниц: ");
+                int pages = Convert.ToInt32(Console.ReadLine());
+
+                // Создаём новую книгу с введёнными данными и сохраняем в массив
+                books[i] = new Book(title, author, year, pages);
+            }
+            //Выводим информацию о всех книгах
+            Console.WriteLine("\nИнформация о книгах:");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Книга " + (i + 1) + ":");
+                books[i].ShowInfo(); // Вызываем метод для показа информации о книге
+            }
+        }
+    }    
+}
+
