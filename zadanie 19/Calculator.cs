5cc1fda4adc7223c17c0ec7ca3f039783c79fc93
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 18:15:09 +0300

    Add files via upload

---
+++ zadanie 19/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Статический класс Calculator — содержит математические методы для работы с числами
+    /// </summary>
+    static class Calculator
+    {
+        /// <summary>
+        /// Метод сложения двух чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Метод вычитания второго числа из первого
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Метод умножения двух чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Метод деления первого числа на второе с проверкой на деление на ноль
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                // Если второй аргумент равен 0 — выводим ошибку и возвращаем 0
+                Console.WriteLine("Ошибка: деление на ноль!");
+                return 0;
+            }// Иначе выполняем деление
+            return a / b;
+        }
+    }
+}
---
+++ zadanie 19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Сложение: " + a + " + " + b + " = " + Calculator.Add(a, b));
+            Console.WriteLine("Вычитание: " + a + " - " + b + " = " + Calculator.Subtract(a, b));
+            Console.WriteLine("Умножение: " + a + " * " + b + " = " + Calculator.Multiply(a, b));
+            Console.WriteLine("Деление: " + a + " / " + b + " = " + Calculator.Divide(a, b));
+        }
+    }
+}
