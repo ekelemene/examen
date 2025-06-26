f0e229e9f428756220ec22751f3329b2087f4c5c
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 18:53:50 +0300

    Add files via upload

---
+++ zadanie 23/Printer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Определение перечисления (enum), содержащего три цвета
+    /// </summary>
+    enum Colors
+    {
+        Красный, //0
+        Синий,   //1
+        Зелёный  //2
+    }
+    /// <summary>
+    /// Статический класс Printer, содержащий метод Print
+    /// </summary>
+    static class Printer
+    {
+        /// <summary>
+        /// Метод Print принимает строку и номер цвета (в виде int)
+        /// </summary>
+        /// <param name="stroka"></param>
+        /// <param name="color"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void Print(string stroka, int color)
+        {
+            //Проверка: если передан неверный номер цвета, выбрасывается исключение
+            if (color < 0 || color > 2)
+            {
+                throw new ArgumentException("Недопустимый номер цвета!");
+            }
+            // Установка соответствующего цвета текста в консоли
+            if (color == (int)Colors.Красный)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (color == (int)Colors.Синий)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (color == (int)Colors.Зелёный)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            // Печать строки цветным текстом
+            Console.WriteLine(stroka);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ zadanie 23/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите строку: ");
+                string text = Console.ReadLine();
+                Console.Write("Выберите цвет (0 - Красный, 1 - Синий, 2 - Зелёный): ");
+                int color = Convert.ToInt32(Console.ReadLine());
+
+                // Выводим строку выбранным цветом
+                Printer.Print(text, color);
+            }
+            catch (FormatException)
+            {
+                // Обрабатываем ситуацию, когда пользователь ввёл не число
+                Console.WriteLine("Ошибка: цвет должен быть числом!");
+            }
+            catch (ArgumentException ex)
+            {
+                // Обрабатываем ошибку, если цвет вне допустимого диапазона (не 0, 1 или 2)
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Общая обработка других непредвиденных ошибок
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
