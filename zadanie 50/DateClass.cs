4d1358cd4f6c655da37a4cb7eb06b3947ae689b4
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-27 00:29:28 +0300

    Add files via upload

---
+++ zadanie 50/DateClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Класс для работы с датой, позволяющий хранить дату и получать предыдущий и следующий день.
+    /// </summary>
+    class DateClass
+    {
+        /// <summary>
+        /// Поле для хранения даты
+        /// </summary>
+        private DateTime data;
+
+        /// <summary>
+        ///  Конструктор по умолчанию, инициализирует дату текущим временем.
+        /// </summary>
+        public DateClass()
+        {
+            data = DateTime.Now;
+        }
+
+        /// <summary>
+        ///  Конструктор, инициализирующий дату по заданным году, месяцу и дню.
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="day"></param>
+        public DateClass(int year, int month, int day)
+        {
+            data = new DateTime(year, month, day);
+        }
+
+        /// <summary>
+        /// Свойство для получения и установки даты.
+        /// </summary>
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        /// <summary>
+        ///  Метод для получения даты предыдущего дня.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime PreviousDay()
+        {
+            return data.AddDays(-1);
+        }
+
+        /// <summary>
+        /// Метод для получения даты следующего дня.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime NextDay()
+        {
+            return data.AddDays(1);
+        }
+    }
+}
---
+++ zadanie 50/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Запрос года у пользователя
+                Console.Write("Введите год: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите месяц: ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите день: ");
+                int day = Convert.ToInt32(Console.ReadLine());
+
+                // Создаем объект DateClass с указанной датой
+                DateClass date = new DateClass(year, month, day);
+                // Выводим текущую дату в формате дд.мм.гггг
+                Console.WriteLine($"Текущая дата: {date.Data:dd.MM.yyyy}");
+                // Выводим дату предыдущего дня
+                Console.WriteLine($"Предыдущий день: {date.PreviousDay():dd.MM.yyyy}");
+                // Выводим дату следующего дня
+                Console.WriteLine($"Следующий день: {date.NextDay():dd.MM.yyyy}");
+            }
+            catch (FormatException)
+            {
+                // Обработка ошибки, если введено не число
+                Console.WriteLine("Ошибка: введено не число!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // Обработка ошибки, если введена недопустимая дата
+                Console.WriteLine("Ошибка: недопустимая дата!");
+            }
+            catch (Exception ex)
+            {
+                // Общая обработка любых других исключений
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
