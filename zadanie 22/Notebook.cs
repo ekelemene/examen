fb4d54228b0c0836389e8eaffb24d78ecb9985cf
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 18:40:57 +0300

    Add files via upload

---
+++ zadanie 22/Notebook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Объявление структуры Notebook (структура — это тип значений, как int или double)
+    /// </summary>
+    struct Notebook
+    {
+        /// <summary>
+        /// Приватные поля, содержащие информацию о ноутбуке
+        /// </summary>
+        private string model;
+        private string manufacturer;
+        private double price;
+
+        /// <summary>
+        /// Пользовательский конструктор, инициализирующий все поля
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="manufacturer"></param>
+        /// <param name="price"></param>
+        public Notebook(string model, string manufacturer, double price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        /// <summary>
+        /// Метод для отображения информации о ноутбуке
+        /// </summary>
+        public void ShowInfo()
+        {
+            Console.WriteLine("Модель: " + model);
+            Console.WriteLine("Производитель: " + manufacturer);
+            Console.WriteLine("Цена: " + String.Format("{0:F2}", price));
+        }
+    }
+}
---
+++ zadanie 22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите модель ноутбука: ");
+                string model = Console.ReadLine();
+                Console.Write("Введите производителя: ");
+                string manufacturer = Console.ReadLine();
+                Console.Write("Введите цену: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                //Создаём экземпляр структуры Notebook с введёнными данными
+                Notebook notebook = new Notebook(model, manufacturer, price);
+                //Выводим информацию о ноутбуке
+                Console.WriteLine("\nИнформация о ноутбуке:");
+                notebook.ShowInfo();
+            }
+            // Обработка исключения, если пользователь ввёл некорректное число
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: цена должна быть числом!");
+            }
+            // Общая обработка всех других исключений
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
