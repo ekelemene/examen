9ca48bcc12555b7e32646586fce2954ace697b9b
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 23:47:47 +0300

    Add files via upload

---
+++ zadanie41/Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Объявление обобщённого класса Point с параметром типа T
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Point<T>
+    {
+        //акрытые поля для хранения координат точки
+        private T x;
+        private T y;
+
+        /// <summary>
+        /// Свойство для доступа к координате X
+        /// </summary>
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        /// <summary>
+        /// Свойство для доступа к координате Y
+        /// </summary>
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        /// <summary>
+        /// Конструктор, инициализирующий координаты точки
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Метод для вывода координ
+        /// </summary>
+        public void Show()
+        {
+            Console.WriteLine($"Точка: ({x}, {y})");
+        }
+    }
+}
---
+++ zadanie41/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Создаём точку с координатами типа int
+                Point<int> intPoint = new Point<int>(3, 4);
+                Console.WriteLine("Точка с int:");
+                intPoint.Show();
+
+                // Создаём точку с координатами типа uint (беззнаковое целое)
+                Point<uint> uintPoint = new Point<uint>(5, 6);
+                Console.WriteLine("\nТочка с uint:");
+                uintPoint.Show();
+
+                // // Создаём точку с координатами типа double (числа с плавающей точкой)
+                Point<double> doublePoint = new Point<double>(2.5, 3.7);
+                Console.WriteLine("\nТочка с double:");
+                doublePoint.Show();
+            }
+            catch (Exception ex)
+            {
+                // Обработка возможных исключений
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
