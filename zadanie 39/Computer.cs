dae612b4dfa42ca3961e72df17ec09f5dfeaadf1
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 23:41:19 +0300

    Add files via upload

---
+++ zadanie 39/Computer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    ///  кдасс компьютер
+    /// </summary>
+    class Computer
+    {
+        /// <summary>
+        /// приватные поля
+        /// </summary>
+        private string model;
+        private int ram;
+        private int hdd;
+
+        /// <summary>
+        /// // Свойство для доступа к модели компьютера
+        /// </summary>
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        /// <summary>
+        /// Свойство для доступа и установки объема RAM
+        /// </summary>
+        public int Ram
+        {
+            get { return ram; }
+            set
+            {
+                // Проверка, что значение RAM находится в допустимом диапазоне (2-32 ГБ)
+                if (value >= 2 && value <= 32)
+                {
+                    ram = value;
+                }
+                else
+                {
+                    // Генерация исключения при недопустимом значении RAM
+                    throw new ArgumentException("ОЗУ должно быть от 2 до 32 ГБ!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Свойство для доступа и установки объема HDD
+        /// </summary>
+        public int Hdd
+        {
+            get { return hdd; }
+            set
+            {
+                // Проверка, что значение HDD находится в допустимом диапазоне (200-2000 ГБ)
+                if (value >= 200 && value <= 2000)
+                {
+                    hdd = value;
+                }
+                else
+                {
+                    // Генерация исключения при недопустимом значении HDD
+                    throw new ArgumentException("HDD должно быть от 200 до 2000 ГБ!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// // Конструктор класса, инициализирует все поля с проверками через свойства
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="ram"></param>
+        /// <param name="hdd"></param>
+        public Computer(string model, int ram, int hdd)
+        {
+            this.model = model;
+            Ram = ram;
+            Hdd = hdd;
+        }
+
+        /// <summary>
+        /// Метод для вычисления стоимости компьютера по формуле:
+        /// базовая цена 500 + 50 за каждый ГБ RAM + 0.5 за каждый ГБ HDD
+        /// </summary>
+        /// <returns></returns>
+        public double CalculateCost()
+        {
+            return 500 + ram * 50 + hdd * 0.5;
+        }
+    }
+}
---
+++ zadanie 39/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Создаем массив компьютеров с разными конфигурациями
+                Computer[] computers = new Computer[]
+                {
+                    new Computer("Model1", 8, 500),
+                    new Computer("Model2", 16, 1000),
+                    new Computer("Model3", 32, 2000)
+                };
+
+                double totalCost = 0; // Переменная для накопления общей стоимости всех компьютеров
+
+                int i; // Перебираем каждый компьютер в массиве
+                for (i = 0; i < computers.Length; i++)
+                {
+                    // Вычисляем стоимость текущего компьютера
+                    double cost = computers[i].CalculateCost();
+                    // Добавляем стоимость к общей сумме
+                    totalCost += cost;
+                    // Выводим информацию о текущем компьютере
+                    Console.WriteLine("Модель: " + computers[i].Model);
+                    Console.WriteLine("ОЗУ: " + computers[i].Ram + " ГБ");
+                    Console.WriteLine("HDD: " + computers[i].Hdd + " ГБ");
+                    Console.WriteLine("Стоимость: " + String.Format("{0:F2}", cost));
+                    Console.WriteLine();
+                }
+                // Выводим общую стоимость всех компьютеров
+                Console.WriteLine("Общая стоимость: " + String.Format("{0:F2}", totalCost));
+            }
+            catch (ArgumentException ex)
+            {
+                // Обрабатываем исключения при неправильных параметрах RAM или HDD
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                //// Общий обработчик других возможных исключений
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
