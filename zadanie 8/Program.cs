b2b92febe8bc65c55860054cfdd4007289b3e4e3
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 14:56:20 +0300

    Add files via upload

---
+++ zadanie 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод и сохранение
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            //Создание нового объекта класса User,
+            User user = new User(login, name, surname, age);
+            //Вызов метода ShowInfo
+            user.ShowInfo();
+        }
+    }
+}
---
+++ zadanie 8/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Класс User, представляющий пользователя
+    /// </summary>
+    class User
+    {
+        //Закрытые поля (private fields) для хранения данных пользователя.
+        private string login;
+        private string name;
+        private string surname;
+        private int age;
+        /// <summary>
+        /// Закрытое поле с модификатором readonly, которое хранит дату заполнения анкеты.
+        /// readonly означает, что значение можно установить только в конструкторе.
+        /// </summary>
+        private readonly DateTime dateFilled;
+
+        /// <summary>
+        /// Конструктор класса User, инициализирующий данные пользователя
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="age"></param>
+        public User(string login, string name, string surname, int age)
+        {
+            this.login = login;
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.dateFilled = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Метод для вывода информации о пользователе в консоль.
+        /// </summary>
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Логин: {login}");
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Фамилия: {surname}");
+            Console.WriteLine($"Возраст: {age}");
+            // Вывод даты заполнения анкеты в формате день.месяц.год 
+            // Формат :dd.MM.yyyy задает отображение даты в нужном виде.
+            Console.WriteLine($"Дата заполнения: {dateFilled:dd.MM.yyyy}");
+        }
+    }
+}
