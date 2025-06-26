f6ab6c3265142765021cb2410054bd04232c54f9
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 18:11:35 +0300

    Add files via upload

---
+++ zadanie 18/Figura.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Класс Figura представляет геометрическую фигуру и умеет считать площадь для разных типов фигур
+    /// </summary>
+    class Figura
+    {
+        /// <summary>
+        /// Метод вычисляет площадь квадрата, принимает длину стороны 'a'
+        /// </summary>
+        /// <param name="a"></param>
+        public void ShowArea(double a)
+        {
+            double area = a * a;
+            Console.WriteLine("Фигура: Квадрат");
+            Console.WriteLine("Площадь: " + String.Format("{0:F2}", area));
+        }
+
+        /// <summary>
+        /// // Перегруженный метод для вычисления площади прямоугольника, принимает длины двух сторон
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void ShowArea(double a, double b)
+        {
+            double area = a * b;
+            Console.WriteLine("Фигура: Прямоугольник");
+            Console.WriteLine("Площадь: " + String.Format("{0:F2}", area));
+        }
+
+        /// <summary>
+        /// Ещё один перегруженный метод для площади трапеции, принимает два основания и высоту
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="h"></param>
+        public void ShowArea(double a, double b, double h)
+        {
+            double area = (a + b) * h / 2;
+            Console.WriteLine("Фигура: Трапеция");
+            Console.WriteLine("Площадь: " + String.Format("{0:F2}", area));
+        }
+    }
+}
---
+++ zadanie 18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаём объект класса Figura для вызова методов вычисления площади
+            Figura figura = new Figura();
+
+            // Площадь квадрата
+            Console.Write("Введите сторону квадрата: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            figura.ShowArea(a);
+
+            // Площадь прямоугольника
+            Console.Write("\nВведите длину прямоугольника: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину прямоугольника: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            figura.ShowArea(a, b);
+
+            // Площадь трапеции
+            Console.Write("\nВведите длину первого основания трапеции: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите длину второго основания трапеции: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите высоту трапеции: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            figura.ShowArea(a, b, h);
+        }
+    }
+}
