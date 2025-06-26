e031db757fc8050f82eee7a7cafe1ea3e86d04b1
ekelemene <ksuxaskrip2007@gmail.com>
2025-06-26 14:26:28 +0300

    Add files via upload

---
+++ zadanie 5/Converter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    /// <summary>
+    /// Класс для конвертации валют между BYN 
+    /// </summary>
+    class Converter
+    {
+        /// <summary>
+        /// поля  курс валют
+        /// </summary>
+        private double usdRate, eurRate, rubRate;
+
+        /// <summary>
+        /// Конструктор, инициализирует курсы валют
+        /// </summary>
+        /// <param name="usd"></param>
+        /// <param name="eur"></param>
+        /// <param name="rub"></param>
+        public Converter(double usd, double eur, double rub)
+        {
+            usdRate = usd;
+            eurRate = eur;
+            rubRate = rub;
+        }
+
+        /// <summary>
+        ///  Конвертация BYN в USD (делим сумму в BYN на курс USD)
+        /// </summary>
+        /// <param name="byn"></param>
+        /// <returns></returns>
+        public double ConvertBYNtoUSD(double byn)
+        {
+            return byn / usdRate;
+        }
+
+        /// <summary>
+        /// Конвертация BYN в EUR
+        /// </summary>
+        /// <param name="byn"></param>
+        /// <returns></returns>
+        public double ConvertBYNtoEUR(double byn)
+        {
+            return byn / eurRate;
+        }
+
+        /// <summary>
+        /// Конвертация BYN в RUB
+        /// </summary>
+        /// <param name="byn"></param>
+        /// <returns></returns>
+        public double ConvertBYNtoRUB(double byn)
+        {
+            return byn / rubRate;
+        }
+
+        /// <summary>
+        /// Конвертация USD в BYN (умножаем сумму USD на курс USD)
+        /// </summary>
+        /// <param name="usd"></param>
+        /// <returns></returns>
+        public double ConvertUSDtoBYN(double usd)
+        {
+            return usd * usdRate;
+        }
+
+        /// <summary>
+        /// Конвертация EUR в BYN
+        /// </summary>
+        /// <param name="eur"></param>
+        /// <returns></returns>
+        public double ConvertEURtoBYN(double eur)
+        {
+            return eur * eurRate;
+        }
+
+        /// <summary>
+        /// Конвертация RUB в BYN
+        /// </summary>
+        /// <param name="rub"></param>
+        /// <returns></returns>
+        public double ConvertRUBtoBYN(double rub)
+        {
+            return rub * rubRate;
+        }
+    }
+}
---
+++ zadanie 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаём объект Converter с заданными курсами валют к BYN
+            Converter converter = new Converter(2.5, 3.0, 0.035);
+
+            //Запрашиваем у пользователя сумму в BYN для конвертации
+            Console.Write("Введите сумму в BYN: ");
+            double byn = Convert.ToDouble(Console.ReadLine());
+            // Конвертируем введённую сумму BYN в USD, EUR и RUB и выводим результат с двумя знаками после запятой
+            Console.WriteLine($"BYN в USD: {converter.ConvertBYNtoUSD(byn):F2}");
+            Console.WriteLine($"BYN в EUR: {converter.ConvertBYNtoEUR(byn):F2}");
+            Console.WriteLine($"BYN в RUB: {converter.ConvertBYNtoRUB(byn):F2}");
+
+            // Запрашиваем у пользователя сумму в USD для конвертации в BYN
+            Console.Write("Введите сумму в USD: ");
+            double usd = Convert.ToDouble(Console.ReadLine());
+            // Конвертируем USD в BYN и выводим результат
+            Console.WriteLine($"USD в BYN: {converter.ConvertUSDtoBYN(usd):F2}");
+
+            // Запрашиваем сумму в EUR для конвертации в BYN
+            Console.Write("Введите сумму в EUR: ");
+            double eur = Convert.ToDouble(Console.ReadLine());
+            // Конвертируем EUR в BYN и выводим результат
+            Console.WriteLine($"EUR в BYN: {converter.ConvertEURtoBYN(eur):F2}");
+
+            // Запрашиваем сумму в RUB для конвертации в BYN
+            Console.Write("Введите сумму в RUB: ");
+            double rub = Convert.ToDouble(Console.ReadLine());
+            // Конвертируем RUB в BYN и выводим результат
+            Console.WriteLine($"RUB в BYN: {converter.ConvertRUBtoBYN(rub):F2}");
+        }
+    }
+}
